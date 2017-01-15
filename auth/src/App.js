b6c48caede9578197bc9dd9ab51dc535116c6f79
Adrian Cotfas <adrcotfas@gmail.com>
2017-01-15 23:59:15 +0200

    include firebase and add LoginForm

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyDi78kw6k8mj7D0ePpGemoJV-l6sGFL2vs',
+    authDomain: 'authentification-6cd2c.firebaseapp.com',
+    databaseURL: 'https://authentification-6cd2c.firebaseio.com',
+    storageBucket: 'authentification-6cd2c.appspot.com',
+    messagingSenderId: '651556818486'
+  });
+  }
+
   render() {
     return (
       <View>
-        <Text>An app</Text>
+        <Header headerText='Authentification' />
+        <LoginForm />
       </View>
     );
   }
