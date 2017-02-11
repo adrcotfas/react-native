e920a72952a9e8590b2bb6d70a7bf50e73d7d580
Adrian Cotfas <adrcotfas@gmail.com>
2017-02-11 22:05:57 +0200

    create user accounts

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -2,8 +2,9 @@ import firebase from 'firebase';
 
 import { 
   EMAIL_CHANGED,
-  PASSWORD_CHANGED
-  LOGIN_USER_SUCCESS
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAILED
 } from './types';
 
 export const emailChanged = (text) => {
@@ -23,8 +24,22 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(user => {
-      dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+    .then(user => loginUserSuccess(dispatch, user))
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => loginUserFailed(dispatch));
     });
   };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
+};
+
+const loginUserFailed = (dispatch) => {
+  dispatch({ type: LOGIN_USER_FAILED });
 };
